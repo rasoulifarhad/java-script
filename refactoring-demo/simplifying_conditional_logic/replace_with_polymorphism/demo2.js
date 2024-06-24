75ab09d129e03d3a42970120173cf55ac3b25169
farhad rasouli <rasouli.farhad@gmail.com>
2024-06-25 03:05:54 +0330

    updated

--- refactoring-demo/simplifying_conditional_logic/replace_with_polymorphism/demo2.js
+++ refactoring-demo/simplifying_conditional_logic/replace_with_polymorphism/demo2.js
@@ -8,30 +8,10 @@ function speed(birds) {
 
 function plumage(bird) {
     return new Bird(bird).plumage();
-    // switch (bird.type) {
-    //     case 'EuropeanSwallow':
-    //         return "average";
-    //     case 'AfricanSwallow':
-    //         return (bird.numberOfCoconuts > 2) ? "tired" :  "average";
-    //     case 'NorwegianBlueParrot':
-    //         return (bird.voltage > 100) ? "scorched" : "beautiful";
-    //     default:
-    //         return "unknown";
-    // }
 }
 
 function airSpeedVelocity(bird) {
     return new Bird(bird).airSpeedVelocity();
-    // switch (bird.type) {
-    //     case 'EuropeanSwallow':
-    //         return 35;
-    //     case 'AfricanSwallow':
-    //         return 40 - 2 * bird.numberOfCoconuts;
-    //     case 'NorwegianBlueParrot':
-    //         return (bird.isNailed) ? 0 : 10 + bird.voltage / 10;
-    //     default:
-    //         return null;
-    // }
 }
 
 class Bird {
@@ -41,10 +21,29 @@ class Bird {
     }
 
     get plumage() {
-
+        switch (this.type) {
+            case 'EuropeanSwallow':
+                return "average";
+            case 'AfricanSwallow':
+                return (this.numberOfCoconuts > 2) ? "tired" :  "average";
+            case 'NorwegianBlueParrot':
+                return (this.voltage > 100) ? "scorched" : "beautiful";
+            default:
+                return "unknown";
+        }
+    
     }
 
     get airSpeedVelocity() {
-
+        switch (this.type) {
+            case 'EuropeanSwallow':
+                return 35;
+            case 'AfricanSwallow':
+                return 40 - 2 * this.numberOfCoconuts;
+            case 'NorwegianBlueParrot':
+                return (this.isNailed) ? 0 : 10 + this.voltage / 10;
+            default:
+                return null;
+        }
     }
 }
