97c217d63fba8fc514804a257cd591754acaf2a0
farhad rasouli <rasouli.farhad@gmail.com>
2024-06-24 23:46:41 +0330

    updated

--- refactoring-demo/simplifying_conditional_logic/guard_clauses/demo2.js
+++ refactoring-demo/simplifying_conditional_logic/guard_clauses/demo2.js
@@ -1,18 +1,17 @@
 function payAmount(employee) {
     let result;
     if(employee.isSeperated) {
-        result = {amount:0, reasonCode:"SEP"};
+        return {amount:0, reasonCode:"SEP"};
+    }
+    if(employee.isRetired) {
+        result = {amount: 0, reasonCode: "RET"};
     } else {
-        if(employee.isRetired) {
-            result = {amount: 0, reasonCode: "RET"};
-        } else {
-            // logic to compute amount
-            lorem.ipsum(dolor.sitAmet);
-            consectetur(adipiscing).elit();
-            sed.do.eiusmod = tempor.incididunt.ut(labore) && dolore(magna.aliqua);
-            ut.enim.ad(minim.veniam);
-            result = someFinalComputation();
-        }
+        // logic to compute amount
+        lorem.ipsum(dolor.sitAmet);
+        consectetur(adipiscing).elit();
+        sed.do.eiusmod = tempor.incididunt.ut(labore) && dolore(magna.aliqua);
+        ut.enim.ad(minim.veniam);
+        result = someFinalComputation();
     }
     return result;
 }
