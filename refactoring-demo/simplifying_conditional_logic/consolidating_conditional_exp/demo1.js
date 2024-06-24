f55f84ad54a44380ee2cb9d5429f706a8582c0cc
farhad rasouli <rasouli.farhad@gmail.com>
2024-06-24 22:58:40 +0330

    updated

--- refactoring-demo/simplifying_conditional_logic/consolidating_conditional_exp/demo1.js
+++ refactoring-demo/simplifying_conditional_logic/consolidating_conditional_exp/demo1.js
@@ -11,5 +11,11 @@ function disabilityAmount(anEmployee) {
         return 0;
     }
 
+    if(anEmployee.onVacation) {
+        if(anEmployee.seniority > 10) {
+            return 1;
+        }
+    }
+     return 0.5;
     // ....
 }
