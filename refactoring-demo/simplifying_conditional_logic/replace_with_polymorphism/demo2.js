17105a3010d40a6ca490007276c640062f2af611
farhad rasouli <rasouli.farhad@gmail.com>
2024-06-25 03:22:20 +0330

    update

--- refactoring-demo/simplifying_conditional_logic/replace_with_polymorphism/demo2.js
+++ refactoring-demo/simplifying_conditional_logic/replace_with_polymorphism/demo2.js
@@ -31,16 +31,28 @@ class EuropeanSwallow extends Bird{
     get plumage() {
         return "average";
     }
+
+    get airSpeedVelocity() {
+        return 35;
+    }
 }
 
 class AfricanSwallow extends Bird{
     get plumage(){
         return (this.numberOfCoconuts > 2) ? "tired" :  "average";
     }
+   
+    get airSpeedVelocity() {
+        return 40 - 2 * this.numberOfCoconuts;        
+    }
 }
 class NorwegianBlueParrot extends Bird{
     get plumage() {
         return (this.voltage > 100) ? "scorched" : "beautiful";
+    }
+   
+    get airSpeedVelocity() {
+        return (this.isNailed) ? 0 : 10 + this.voltage / 10;
     }
 }
 
@@ -55,15 +67,6 @@ class Bird {
     }
 
     get airSpeedVelocity() {
-        switch (this.type) {
-            case 'EuropeanSwallow':
-                return 35;
-            case 'AfricanSwallow':
-                return 40 - 2 * this.numberOfCoconuts;
-            case 'NorwegianBlueParrot':
-                return (this.isNailed) ? 0 : 10 + this.voltage / 10;
-            default:
-                return null;
-        }
+        return null;
     }
 }
