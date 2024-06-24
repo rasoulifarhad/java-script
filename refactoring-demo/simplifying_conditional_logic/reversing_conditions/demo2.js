4e67869197e18c556c00dc5797a2aa2c35484a68
farhad rasouli <rasouli.farhad@gmail.com>
2024-06-24 23:59:00 +0330

    updated

--- refactoring-demo/simplifying_conditional_logic/reversing_conditions/demo2.js
+++ refactoring-demo/simplifying_conditional_logic/reversing_conditions/demo2.js
@@ -5,7 +5,7 @@ function adjustedCapital(anInstrument) {
         return result;
     }
     
-    if (!(anInstrument.interestRate > 0 && anInstrument.duration > 0)) {
+    if ((anInstrument.interestRate <= 0 || anInstrument.duration <= 0)) {
         return result;
     }
     
