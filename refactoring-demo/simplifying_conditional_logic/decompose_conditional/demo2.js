a971426c709bab94dd2f0431c1c77549bd1934d6
farhad rasouli <rasouli.farhad@gmail.com>
2024-06-24 22:37:06 +0330

    updated

--- refactoring-demo/simplifying_conditional_logic/decompose_conditional/demo2.js
+++ refactoring-demo/simplifying_conditional_logic/decompose_conditional/demo2.js
@@ -2,7 +2,7 @@
 var aDate = new Date();
 function calcCharge(aDate) {simplifying_conditional_logic/demo.js
 
-    if(!aDate.isBefore(plan.summerStart) && !aDate.isAfter(plan.summerEnd)) {
+    if(summer()) {
         charge = quantity * plan.summerRate;
     } else {
         charge = quantity * plan.regularRate + plan.regularServiceCharge;
