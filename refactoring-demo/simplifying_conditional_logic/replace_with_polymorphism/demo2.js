016c3df040fc464f1044e800f5ebbf6bf0d2c952
farhad rasouli <rasouli.farhad@gmail.com>
2024-06-25 03:00:26 +0330

    updated

--- refactoring-demo/simplifying_conditional_logic/replace_with_polymorphism/demo2.js
+++ refactoring-demo/simplifying_conditional_logic/replace_with_polymorphism/demo2.js
@@ -7,27 +7,29 @@ function speed(birds) {
 }
 
 function plumage(bird) {
-    switch (bird.type) {
-        case 'EuropeanSwallow':
-            return "average";
-        case 'AfricanSwallow':
-            return (bird.numberOfCoconuts > 2) ? "tired" :  "average";
-        case 'NorwegianBlueParrot':
-            return (bird.voltage > 100) ? "scorched" : "beautiful";
-        default:
-            return "unknown";
-    }
+    return new Bird(bird).plumage();
+    // switch (bird.type) {
+    //     case 'EuropeanSwallow':
+    //         return "average";
+    //     case 'AfricanSwallow':
+    //         return (bird.numberOfCoconuts > 2) ? "tired" :  "average";
+    //     case 'NorwegianBlueParrot':
+    //         return (bird.voltage > 100) ? "scorched" : "beautiful";
+    //     default:
+    //         return "unknown";
+    // }
 }
 
 function airSpeedVelocity(bird) {
-    switch (bird.type) {
-        case 'EuropeanSwallow':
-            return 35;
-        case 'AfricanSwallow':
-            return 40 - 2 * bird.numberOfCoconuts;
-        case 'NorwegianBlueParrot':
-            return (bird.isNailed) ? 0 : 10 + bird.voltage / 10;
-        default:
-            return null;
-    }
+    return new Bird(bird).airSpeedVelocity();
+    // switch (bird.type) {
+    //     case 'EuropeanSwallow':
+    //         return 35;
+    //     case 'AfricanSwallow':
+    //         return 40 - 2 * bird.numberOfCoconuts;
+    //     case 'NorwegianBlueParrot':
+    //         return (bird.isNailed) ? 0 : 10 + bird.voltage / 10;
+    //     default:
+    //         return null;
+    // }
 }
