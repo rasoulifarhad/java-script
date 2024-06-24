7b7e4fdd84f3f0114b3d37f7a8375de7dd391d65
farhad rasouli <rasouli.farhad@gmail.com>
2024-06-25 00:08:48 +0330

    updated

--- refactoring-demo/simplifying_conditional_logic/reversing_conditions/demo2.js
+++ refactoring-demo/simplifying_conditional_logic/reversing_conditions/demo2.js
@@ -1,12 +1,10 @@
 // Replace Nested Conditional with Guard Clauses by reversing the conditional expressions. 
 function adjustedCapital(anInstrument) {
-    let result = 0;
     if ( (anInstrument.capital <= 0)
         || (anInstrument.interestRate <= 0 )
         || (anInstrument.duration <= 0)) {
-        return result;
+        return 0;
     }
     
-    result = (anInstrument.income / anInstrument.duration) * anInstrument.adju
-    return result;
+    return (anInstrument.income / anInstrument.duration) * anInstrument.adju
 }
