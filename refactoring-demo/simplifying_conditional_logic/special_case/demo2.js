8a9d9287bba8502fcb0310ef42912c19f7088746
farhad rasouli <rasouli.farhad@gmail.com>
2024-06-29 23:33:33 +0330

    updated

---
+++ refactoring-demo/simplifying_conditional_logic/special_case/demo2.js
@@ -0,0 +1,53 @@
+class Site{
+    get customer() {
+        return this._customer;
+    }
+}
+
+class Customer {
+
+    get name() {
+
+    }
+
+    get billingPlan() {
+
+    }
+
+    set billingPlan(arg) {
+
+    }
+
+    get paymentHistory() {
+
+    }
+}
+
+function client1() {
+    const aCustomer = site.customer;
+    let customerName;
+    if(aCustomer === "unknown") {
+        customerName = "occupant";
+    } else {
+        customerName = aCustomer.name;
+    }
+}
+
+function client2() {
+    const plan = (aCustomer === "unknown") ? 
+        registry.billingPlans.basic :
+        aCustomer.billingPlan;
+}
+
+function client3() {
+    if (aCustomer != "unknown") {
+        aCustomer.billingPlan = newPlan;
+    }
+}
+
+
+function client4(){
+    const weeksDelinquent = (aCustomer === "unknown") ?
+        0
+        : aCustomer.paymentHistory.weeksDelinquentInLastYear;
+}
