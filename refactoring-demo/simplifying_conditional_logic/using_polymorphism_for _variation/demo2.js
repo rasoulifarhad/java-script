ba36e1dcbd4e80f6773cd40041a8d4277e2ed7c5
farhad rasouli <rasouli.farhad@gmail.com>
2024-06-29 21:58:25 +0330

    updated

--- refactoring-demo/simplifying_conditional_logic/using_polymorphism_for _variation/demo2.js
+++ refactoring-demo/simplifying_conditional_logic/using_polymorphism_for _variation/demo2.js
@@ -1,73 +1,88 @@
-function raiting(voyage, history) {
-    const vpf = voyagePrifitFactor(voyage, history);
-    const vr = voyageRisk(voyage);
-    const chr = capitalHistoryRisk(voyage, history);
-    if (vpf * 3 > (vr + chr * 2)) {
-        return "A";
-    }
-    return "B";
+function raitingOf(voyage, history) {
+    return new Raiting(voyage, history).value;
 }
 
-function voyageRisk(voyage) {
-    let result = 1;
-    if (voyage.length > 4) {
-        result += 2;
-    }
-    if (voyage.length > 8) {
-        result += voyage.length  - 8;
-    }
-    if (["china", "east-indies"].includes(voyage.zone)) {
-        result += 4;
-    }
-    return Math.max(result, 0);
-}
+class Raiting {
 
-function capitalHistoryRisk(voyage, history) {
-    let result = 1;
-    if (history.length < 5) {
-        result += 4;
+    constructor(voyage, history) {
+        this.voyage = voyage;
+        this.history = history;
     }
-    result += history.filter(v => v.profit < 0).length;
-    if (voyage.zone === "china" && hasChina(history)) {
-        result -= 2;
-    }
-    return Math.max(result, 0);
-}
 
-function hasChina(history) {
-    return history.some(v => "china" === v.zone);
-}
+    get value() {
+        const vpf = this.voyagePrifitFactor();
+        const vr = this.voyageRisk();
+        const chr = this.capitalHistoryRisk();
+        if (vpf * 3 > (vr + chr * 2)) {
+            return "A";
+        }
+        return "B";
+    }
 
-function voyagePrifitFactor(voyage, history) {
-    let result = 2;
-    if (voyage.zone === "china") {
-        result += 1;
+    get voyageRisk() {
+        let result = 1;
+        if (this.voyage.length > 4) {
+            result += 2;
+        }
+        if (this.voyage.length > 8) {
+            result += this.voyage.length  - 8;
+        }
+        if (["china", "east-indies"].includes(this.voyage.zone)) {
+            result += 4;
+        }
+        return Math.max(result, 0);
     }
-    if (voyage.zone === "east-indies") {
-        result += 1;
+    
+    get capitalHistoryRisk() {
+        let result = 1;
+        if (this.history.length < 5) {
+            result += 4;
+        }
+        result += this.history.filter(v => v.profit < 0).length;
+        if (this.voyage.zone === "china" && this.hasChinaHistory()) {
+            result -= 2;
+        }
+        return Math.max(result, 0);
     }
-    if (voyage.zone === "china" && hasChina(history)) {
-        result += 3;
-        if (history.length > 10) {
+    
+    get hasChinaHistory() {
+        return this.history.some(v => "china" === v.zone);
+    }
+    
+    get voyagePrifitFactor() {
+        let result = 2;
+        if (this.voyage.zone === "china") {
             result += 1;
         }
-        if (voyage.length > 12) {
+        if (this.voyage.zone === "east-indies") {
             result += 1;
         }
-        if (voyage.length > 18) {
-            result  -= 1;
+        if (this.voyage.zone === "china" && this.hasChinaHistory()) {
+            result += 3;
+            if (this.history.length > 10) {
+                result += 1;
+            }
+            if (this.voyage.length > 12) {
+                result += 1;
+            }
+            if (this.voyage.length > 18) {
+                result  -= 1;
+            }
         }
+        else {
+            if (this.history.length > 8) {
+                result += 1;
+            }
+            if (this.voyage.length > 14) {
+                result  -= 1;
+            }
+        }
+        return result;        
     }
-    else {
-        if (history.length > 8) {
-            result += 1;
-        }
-        if (voyage.length > 14) {
-            result  -= 1;
-        }
-    }
-    return result;        
+    
 }
+
+
 
 
 function main() {
@@ -78,5 +93,5 @@ function main() {
         {zone: "china", profit: -2},
         {zone: "west-africa", profit: 7},
     ];
-    const myRating = rating(voyage, history);
+    const myRating = raitingOf(voyage, history);
 }
