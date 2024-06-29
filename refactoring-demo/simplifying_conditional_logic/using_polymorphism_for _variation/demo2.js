76d84654973ba888f507b1de5d5a50eabe5d8de1
farhad rasouli <rasouli.farhad@gmail.com>
2024-06-29 22:30:15 +0330

    updated

--- refactoring-demo/simplifying_conditional_logic/using_polymorphism_for _variation/demo2.js
+++ refactoring-demo/simplifying_conditional_logic/using_polymorphism_for _variation/demo2.js
@@ -60,29 +60,13 @@ class Raiting {
     
     get voyageAndHistoryLengthFactor() {
         let result = 0;
-        if (this.voyage.zone === "china" && this.hasChinaHistory()) {
-            result += 3;
-            if (this.history.length > 10) {
-                result += 1;
-            }
-            if (this.voyage.length > 12) {
-                result += 1;
-            }
-            if (this.voyage.length > 18) {
-                result  -= 1;
-            }
+        if (this.history.length > 8) {
+            result += 1;
         }
-        else {
-            if (this.history.length > 8) {
-                result += 1;
-            }
-            if (this.voyage.length > 14) {
-                result  -= 1;
-            }
+        if (this.voyage.length > 14) {
+            result  -= 1;
         }
-        return result;        
-
-
+       return result;        
     }
 }
 
@@ -92,6 +76,22 @@ class ExperientedChinaRaiting extends Raiting {
         const result = super.capitalHistoryRisk() - 2;
         return Math.max(result, 0);
     }
+
+    get voyageAndHistoryLengthFactor() {
+        let result = 0;
+        result += 3;
+        if (this.history.length > 10) {
+            result += 1;
+        }
+        if (this.voyage.length > 12) {
+            result += 1;
+        }
+        if (this.voyage.length > 18) {
+            result  -= 1;
+        }
+        return result;        
+    }
+
 
 }
 
