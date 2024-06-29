6ae73b637b108b3def54c31e02ea55b88c687862
farhad rasouli <rasouli.farhad@gmail.com>
2024-06-29 20:15:09 +0330

    updaed

--- refactoring-demo/simplifying_conditional_logic/using_polymorphism_for _variation/demo2.js
+++ refactoring-demo/simplifying_conditional_logic/using_polymorphism_for _variation/demo2.js
@@ -10,17 +10,27 @@ function raiting(voyage, history) {
 
 function voyageRisk(voyage) {
     let result = 1;
-    if (voyage.length > 4) result += 2;
-    if (voyage.length > 8) result += voyage.length  - 8;
-    if (["china", "east­indies"].includes(voyage.zone)) result += 4;
+    if (voyage.length > 4) {
+        result += 2;
+    }
+    if (voyage.length > 8) {
+        result += voyage.length  - 8;
+    }
+    if (["china", "east-indies"].includes(voyage.zone)) {
+        result += 4;
+    }
     return Math.max(result, 0);
 }
 
 function capitalHistoryRisk(voyage, history) {
     let result = 1;
-    if (history.length < 5) result += 4;
+    if (history.length < 5) {
+        result += 4;
+    }
     result += history.filter(v => v.profit < 0).length;
-    if (voyage.zone === "china" && hasChina(history)) result -= 2;
+    if (voyage.zone === "china" && hasChina(history)) {
+        result -= 2;
+    }
     return Math.max(result, 0);
 }
 
@@ -30,17 +40,31 @@ function hasChina(history) {
 
 function voyagePrifitFactor(voyage, history) {
     let result = 2;
-    if (voyage.zone === "china") result += 1;
-    if (voyage.zone === "east­indies") result += 1;
+    if (voyage.zone === "china") {
+        result += 1;
+    }
+    if (voyage.zone === "east-indies") {
+        result += 1;
+    }
     if (voyage.zone === "china" && hasChina(history)) {
         result += 3;
-        if (history.length > 10) result += 1;
-        if (voyage.length > 12) result += 1;
-        if (voyage.length > 18) result  -= 1;
+        if (history.length > 10) {
+            result += 1;
+        }
+        if (voyage.length > 12) {
+            result += 1;
+        }
+        if (voyage.length > 18) {
+            result  -= 1;
+        }
     }
     else {
-        if (history.length > 8) result += 1;
-        if (voyage.length > 14) result  -= 1;
+        if (history.length > 8) {
+            result += 1;
+        }
+        if (voyage.length > 14) {
+            result  -= 1;
+        }
     }
     return result;        
 }
