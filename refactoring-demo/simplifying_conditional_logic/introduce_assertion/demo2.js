b9bed5f308c52b9d9fffbf0e9aa895eb074adad5
farhad rasouli <rasouli.farhad@gmail.com>
2024-07-04 17:03:01 +0330

    updated

--- refactoring-demo/simplifying_conditional_logic/introduce_assertion/demo2.js
+++ refactoring-demo/simplifying_conditional_logic/introduce_assertion/demo2.js
@@ -5,8 +5,10 @@ class Customer {
     }
 
     applyDiscount(aNumber) {
-        return (this.discountRate)
-            ? aNumber - (this.discountRate * aNumber)
-            : aNumber;
+        if(!this.discountRate) {
+            return aNumber;
+        } else {
+            return aNumber - (this.discountRate * aNumber);
+        }
     }
 }
