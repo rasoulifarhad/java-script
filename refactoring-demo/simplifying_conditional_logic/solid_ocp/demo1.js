c9b9fec2f0b2c13b1d099fec2bb36c12b1b7a57c
farhad rasouli <rasouli.farhad@gmail.com>
2024-07-05 00:01:47 +0330

    updated

--- refactoring-demo/simplifying_conditional_logic/solid_ocp/demo1.js
+++ refactoring-demo/simplifying_conditional_logic/solid_ocp/demo1.js
@@ -3,3 +3,35 @@ class PaymentProcessor {
         // Credit card payment logic
     }
 }
+
+class Coder {
+
+    constructor(fullName, language, hobby, education, workplace, position) {
+        this.fullName = fullName;
+        this.language = language;
+        this.hobby = hobby;
+        this.education = education;
+        this.workplace = workplace;
+        this.position = position;
+    }
+}
+
+class CoderFilter {
+
+    filterByName(coders, fullName) {
+        return coders.filter(coder => coder.fullName === fullName)
+    }
+
+    filterBySize(coders, language) {
+        return coders.filter(coder => coder.language === language);
+    }
+
+    filterByHobby(coders, hobby) {
+        return coders.filter(coder => coder.hobby === hobby);
+    }
+}
+
+var coders = [];
+coders.push(new Coder("foo", "java", "coding", "bachelor", "google","developer"));
+var coderFilter = new CoderFilter();
+console.log(coderFilter.filterByName(coders, "foo"));
