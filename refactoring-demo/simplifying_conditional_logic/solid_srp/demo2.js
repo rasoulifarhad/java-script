047349483eb6cec8490218fbd5c78d2b9685ae8d
farhad rasouli <rasouli.farhad@gmail.com>
2024-07-04 23:00:42 +0330

    updated

--- refactoring-demo/simplifying_conditional_logic/solid_srp/demo2.js
+++ refactoring-demo/simplifying_conditional_logic/solid_srp/demo2.js
@@ -12,3 +12,54 @@ class UserAuthentication {
         // authentication logic
     }
 }
+
+class UserManager {
+
+    registerUser(userData) {
+        // registration logic
+    }
+
+    authenticateUser(username, password) {
+        // authentication logic
+    }
+}
+
+var fs = require('fs'); 
+const { todo } = require('node:test');
+class TodoList {
+
+    constructor() {
+        this.items = [];
+    }
+
+    addItem(text) {
+        this.items.push(text);
+    }
+
+    removeItem(index) {
+        this.items = items.splice(index, 1);
+    }
+
+    toString() {
+        return this.items.toString();
+    }
+}
+
+class DatabaseManager {
+
+    saveToFile(data, filename) {
+        fs.writeFileSync(filename, data.toString());
+    }
+
+    loadFromFile(filename) {
+
+    }
+}
+
+var todoList = new TodoList();
+todoList.addItem("foo");
+todoList.addItem("bar");
+console.log(todoList.toString());
+var databaseManager = new DatabaseManager();
+
+databaseManager.saveToFile(todoList, "./todoList.txt");
