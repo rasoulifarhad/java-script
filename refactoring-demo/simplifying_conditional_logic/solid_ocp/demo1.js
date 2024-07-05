bed97ca09bd156159c36fadbccac86364175e7c6
farhad rasouli <rasouli.farhad@gmail.com>
2024-07-05 11:36:27 +0330

    updated

--- refactoring-demo/simplifying_conditional_logic/solid_ocp/demo1.js
+++ refactoring-demo/simplifying_conditional_logic/solid_ocp/demo1.js
@@ -35,3 +35,24 @@ var coders = [];
 coders.push(new Coder("foo", "java", "coding", "bachelor", "google","developer"));
 var coderFilter = new CoderFilter();
 console.log(coderFilter.filterByName(coders, "foo"));
+
+
+class ManageSalaries {
+
+    constructor() {
+        this.salaryRates = [
+            {id: 1, role: 'developer', rate: 100},
+            {id: 2, role: 'architect', rate: 200},
+            {id: 3, role: 'manager', rate: 300},
+        ];
+    }
+
+    calculateSalaries(empId, hoursWorked) {
+        let salaryRate = this.salaryRates.find(s => s.id === empId);
+        return hoursWorked * salaryRate;
+    }
+
+}
+
+const manageSalaries = new ManageSalaries();
+console.log('Salary: ', manageSalaries.calculateSalaries(2, 100));
--- refactoring-demo/simplifying_conditional_logic/solid_ocp/demo2.js
+++ refactoring-demo/simplifying_conditional_logic/solid_ocp/demo2.js
@@ -71,3 +71,28 @@ const filterByProp = (array, propName, value) =>
 var coders = [];
 coders.push(new Coder("foo", "java", "coding", "bachelor", "google","developer"));
 console.log(filterByProp(coders, "fullName", "foo"));
+
+class ManageSalaries {
+
+    constructor() {
+        this.salaryRates = [
+            {id: 1, role: 'developer', rate: 100},
+            {id: 2, role: 'architect', rate: 200},
+            {id: 3, role: 'manager', rate: 300},
+        ];
+    }
+
+    calculateSalaries(empId, hoursWorked) {
+        let salaryRate = this.salaryRates.find(s => s.id === empId);
+        return hoursWorked * salaryRate;
+    }
+
+    addSalaryRate(id, role, rate) {
+        this.salaryRates.push({id: id, role: role, rate: rate});
+    }
+
+}
+
+const manageSalaries = new ManageSalaries();
+manageSalaries.addSalaryRate(4, 's_developer', 250);
+console.log('Salary: ', manageSalaries.calculateSalaries(4, 100));
