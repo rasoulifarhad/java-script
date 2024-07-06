d220eecf8f1759bee8277ea4e57cf2380f0a0890
farhad rasouli <rasouli.farhad@gmail.com>
2024-07-07 02:46:35 +0330

    updated

--- refactoring-demo/simplifying_conditional_logic/seperate_query_from_modifier/demo2.js
+++ refactoring-demo/simplifying_conditional_logic/seperate_query_from_modifier/demo2.js
@@ -2,11 +2,14 @@ function alertForMiscreant(people) {
     for(const p of people) {
         if(p === "Don") {
             setOffAlarms();
+            return;
         }
         if(p === "John") {
             setOffAlarms();
+            return;
         }
     }
+    return;
 }
 
 function findMiscreant(people) {
