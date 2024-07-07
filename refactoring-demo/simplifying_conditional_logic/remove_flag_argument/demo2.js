3e754b2987d2ae97b4a14a8b6e107155064e1ca7
farhad rasouli <rasouli.farhad@gmail.com>
2024-07-07 23:39:13 +0330

    updated

---
+++ refactoring-demo/simplifying_conditional_logic/remove_flag_argument/demo2.js
@@ -0,0 +1,12 @@
+// A flag argument is a function argument that the caller uses to 
+// indicate which logic the called function should execute.
+function bookConcert(aCustomer, isPremium) {
+    if(isPremium) {
+
+    } else {
+
+    }
+}
+
+const aCustomer = {};
+bookConcert(aCustomer, true);
