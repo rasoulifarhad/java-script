c5062a351e548f0ecb914bcbbc90782d8a8ea2b5
farhad rasouli <rasouli.farhad@gmail.com>
2024-07-08 00:52:21 +0330

    updated

--- refactoring-demo/simplifying_conditional_logic/remove_flag_argument/demo2.js
+++ refactoring-demo/simplifying_conditional_logic/remove_flag_argument/demo2.js
@@ -19,3 +19,40 @@ function regularBookConcert(aCustomer) {
 const aCustomer = {};
 
 premiumBookConcert(aCustomer);
+
+
+function deliveryDate(anOrder, isRush) {
+    if (isRush) {
+        let deliveryTime;
+        if (["MA", "CT"].includes(anOrder.deliveryState)) {
+            deliveryTime = 1;
+        } else if (["NY", "NH"].includes(anOrder.deliveryState)) {
+            deliveryTime = 2;
+        } else {
+            deliveryTime = 3;
+        }
+        anOrder.placedOn.setDate(anOrder.placedOn.getDate() + (1 + deliveryTime));
+    } else {
+        let deliveryTime;
+        if (["MA", "CT", "NY"].includes(anOrder.deliveryState)) {
+            deliveryTime = 2;
+        } else if (["ME", "NH"] .includes(anOrder.deliveryState)) {
+            deliveryTime = 3;
+        } else {
+            deliveryTime = 4;
+        }
+        anOrder.placedOn.setDate(anOrder.placedOn.getDate() + (2 + deliveryTime));
+    }  
+    return anOrder.placedOn;
+  
+}
+
+const anOrder = {
+    deliveryState: "NY",
+    placedOn: new Date(),
+};
+const aShipment = {};
+aShipment.deliveryDate = deliveryDate(anOrder, true);
+console.log(aShipment.deliveryDate.toISOString());
+aShipment.deliveryDate = deliveryDate(anOrder, false);
+console.log(aShipment.deliveryDate.toISOString());
