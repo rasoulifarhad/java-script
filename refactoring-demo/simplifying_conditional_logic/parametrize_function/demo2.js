3875b5d3b9203ddd3d5ec9eed32a1ce96d952771
farhad rasouli <rasouli.farhad@gmail.com>
2024-07-07 23:30:20 +0330

    updated

--- refactoring-demo/simplifying_conditional_logic/parametrize_function/demo2.js
+++ refactoring-demo/simplifying_conditional_logic/parametrize_function/demo2.js
@@ -7,7 +7,7 @@ function baseCharge(usage) {
         return usd(0);
     }
     const amount =
-        bottomBand(usage) * 0.03
+        withinBand(usage, 0, 100) * 0.03
         + withinBand(usage, 100, 200) * 0.05
         + topBand(usage) * 0.07;
     return usd(amount);
