df3b5cf4223819a3b39e70deeee1ad42d370bf4d
farhad rasouli <rasouli.farhad@gmail.com>
2024-07-07 03:33:21 +0330

    updated

---
+++ refactoring-demo/simplifying_conditional_logic/parametrize_function/demo2.js
@@ -0,0 +1,9 @@
+function tenPercentRaise(aPerson) {
+    aPerson.salary = aPerson.salary.multiply(1.1);
+
+}
+
+function fivePercentRaise(aPerson) {
+    aPerson.salary = aPerson.salary.multiply(1.05);
+
+}
