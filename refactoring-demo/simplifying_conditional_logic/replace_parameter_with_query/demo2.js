73bd1195a2d4acb4f1a58739e88cd2f9182f69a1
farhad rasouli <rasouli.farhad@gmail.com>
2024-07-09 23:11:37 +0330

    updated

--- refactoring-demo/simplifying_conditional_logic/replace_parameter_with_query/demo2.js
+++ refactoring-demo/simplifying_conditional_logic/replace_parameter_with_query/demo2.js
@@ -7,12 +7,7 @@ class Order {
     
     get finalPrice() {
         const basePrice = this.quantity * this.itemPrice;
-        let discountLevel;
-        if(this.quantity > 100) {
-            discountLevel = 2;
-        } else {
-            discountLevel = 1;
-        }
+        let discountLevel = this.discountLevel();
         return this.discountedPrice(basePrice, discountLevel);
     }
 
