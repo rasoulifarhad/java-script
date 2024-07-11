f85bafceb8e1929d7a2dd4c9b305bdd06e31e687
farhad rasouli <rasouli.farhad@gmail.com>
2024-07-12 01:13:07 +0330

    updated

--- refactoring-demo/simplifying_conditional_logic/refactoring_legacy_code/game-runner.js
+++ refactoring-demo/simplifying_conditional_logic/refactoring_legacy_code/game-runner.js
@@ -2,26 +2,32 @@ var Game = require('./game');
 
 module.exports = function gameRunner(randomInt) {
 
-    function isCurrentAnswerWrong(randomInt, maxAnswerId, wrongAnswerId) {
+    function isCurrentAnswerWrong(maxAnswerId, wrongAnswerId) {
         return randomInt(maxAnswerId) == wrongAnswerId;
     }
-    
-    var notAWinner = false;
-    var game = new Game();
-    game.add('Chet');
-    game.add('Pat');
-    game.add('Sue');
-    const wrongAnswerId = 7;
-    const maxAnswerId = 10;
-    do {
-        const dice = randomInt(6);
-        game.roll(dice);
-        if (isCurrentAnswerWrong(randomInt, maxAnswerId, wrongAnswerId)) {
-            notAWinner = game.wrongAnswer();
-        } else {
-            notAWinner = game.wasCorrectlyAnswered();
-        }
-    } while (notAWinner);
+
+    function run() {
+        var notAWinner = false;
+        var game = new Game();
+        game.add('Chet');
+        game.add('Pat');
+        game.add('Sue');
+        const wrongAnswerId = 7;
+        const maxAnswerId = 10;
+        do {
+            const dice = randomInt(6);
+            game.roll(dice);
+            if (isCurrentAnswerWrong(maxAnswerId, wrongAnswerId)) {
+                notAWinner = game.wrongAnswer();
+            } else {
+                notAWinner = game.wasCorrectlyAnswered();
+            }
+        } while (notAWinner);
+
+    }
+
+    run();
+
 }
 
 
