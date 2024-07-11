98762acc182023a23b42681c82e981346fea5a23
farhad rasouli <rasouli.farhad@gmail.com>
2024-07-11 23:35:36 +0330

    updated

--- refactoring-demo/simplifying_conditional_logic/refactoring_legacy_code/game-runner.js
+++ refactoring-demo/simplifying_conditional_logic/refactoring_legacy_code/game-runner.js
@@ -6,10 +6,11 @@ module.exports = function gameRunner(randomInt) {
     game.add('Chet');
     game.add('Pat');
     game.add('Sue');
+    const wrongAnswerId = 7;
     do {
         const dice = randomInt(6);
         game.roll(dice);
-        if (randomInt(10) == 7) {
+        if (randomInt(10) == wrongAnswerId) {
             notAWinner = game.wrongAnswer();
         } else {
             notAWinner = game.wasCorrectlyAnswered();
