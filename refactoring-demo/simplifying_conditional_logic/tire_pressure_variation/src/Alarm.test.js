1ccc77a6f6a81f771713e8ea16395d3a3aa200b1
farhad rasouli <rasouli.farhad@gmail.com>
2024-07-13 01:57:32 +0330

    updated

---
+++ refactoring-demo/simplifying_conditional_logic/tire_pressure_variation/src/Alarm.test.js
@@ -0,0 +1,9 @@
+import Alarm from "./Alarm"; 
+
+describe("Alarm", function () {
+    test("fixme", function () {
+        const alarm = new Alarm();
+        alarm.check();
+        expect(false).toEqual(true);
+    });
+});
