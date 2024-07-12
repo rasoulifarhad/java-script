3eb6021fa5fe6cc1ef0314dcaded8aba940581d9
farhad rasouli <rasouli.farhad@gmail.com>
2024-07-12 14:10:14 +0330

    updated

--- refactoring-demo/simplifying_conditional_logic/refactoring_legacy_code/game.spec.js
+++ refactoring-demo/simplifying_conditional_logic/refactoring_legacy_code/game.spec.js
@@ -4,35 +4,40 @@ const expect = require('chai').expect;
 const approvals = require('approvals');
 approvals.mocha();
 const _ = require('lodash');
-const {getRandom} = require('./rnd.js');
+const { getRandom } = require('./rnd.js');
 const fs = require('fs');
 
-describe("The test environment", function() {
+describe("The test environment", function () {
 
   let expectedContent;
 
-  beforeEach(function() {
+  beforeEach(function () {
     var data = fs.readFileSync('The_test_environment.should_pass.approved.txt');
     expectedContent = data.toString();
   });
 
-  it("should pass", function() {
+  it("should pass", function () {
     expect(true).to.be.true;
     expect(true).to.be.equal(true)
   });
 
-  it("should access the game", function() {
+  it("should access the game", function () {
     expect(Game).anything;
   });
 
-  it("game should pass", function() {
+  it("should create the game", function () {
+    const game = new Game();
+    expect(Game).anything;
+  });
+
+  it("game should pass", function () {
     const loggedLines = [];
     const oldLog = console.log;
-    console.log = function(arg) {
+    console.log = function (arg) {
       loggedLines.push(arg);
     }
     _.range(18).forEach(() => {
-        gameRunner(getRandom);
+      gameRunner(getRandom);
     });
     console.log = oldLog;
     let actualContent = loggedLines.join("\n");
