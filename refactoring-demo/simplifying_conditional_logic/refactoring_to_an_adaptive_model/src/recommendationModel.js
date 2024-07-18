3c6980910ea9cd02f05a03c40c11070a0d348089
farhad rasouli <rasouli.farhad@gmail.com>
2024-07-19 01:49:39 +0330

    updated

--- refactoring-demo/simplifying_conditional_logic/refactoring_to_an_adaptive_model/src/recommendationModel.js
+++ refactoring-demo/simplifying_conditional_logic/refactoring_to_an_adaptive_model/src/recommendationModel.js
@@ -1,6 +1,13 @@
-export default [
-    {
-        "condition": "atNight",
-        "result": "whispering death"
-    }
-];
+import fs from 'fs';
+
+let model;
+
+export function loadJson() {
+    model = JSON.parse(fs.readFileSync('recommendationModel.json', {encoding: 'utf8'}));
+}
+
+// loadJson caled at some point during application initialization. 
+// this module can have a default export function, which suits its usage after initialization.
+export default function getModel() {
+    return model;
+}
