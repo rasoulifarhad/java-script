4e86d81db2ae8bfbe15e61e616666f0b09cfda5c
farhad rasouli <rasouli.farhad@gmail.com>
2024-07-19 04:02:48 +0330

    updated

--- refactoring-demo/simplifying_conditional_logic/refactoring_to_an_adaptive_model/src/recommender_old.spec.js
+++ refactoring-demo/simplifying_conditional_logic/refactoring_to_an_adaptive_model/src/recommender_old.spec.js
@@ -2,7 +2,7 @@ const expect = require('chai').expect;
 const assert = require('chai').assert;
 const approvals = require('approvals');
 approvals.mocha();
-const executeModel =  require('./recommender_old.js').executeModel;
+const recommender =  require('./recommender_old.js').recommender;
 
 describe("", function() {
 
