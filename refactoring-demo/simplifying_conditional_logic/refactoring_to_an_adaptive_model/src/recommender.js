06b9654843ad636d689a3367d9acb8a9593f992c
farhad rasouli <rasouli.farhad@gmail.com>
2024-07-19 10:59:07 +0330

    updated

--- refactoring-demo/simplifying_conditional_logic/refactoring_to_an_adaptive_model/src/recommender.js
+++ refactoring-demo/simplifying_conditional_logic/refactoring_to_an_adaptive_model/src/recommender.js
@@ -33,13 +33,6 @@ function seasonIncludes(spec, arg) {
 function countryIncludedIn(spec, anArray) {
     return anArray.includes(spec.country);
 }
-// Here you can see the general form of an adaptive model. We have a data structure 
-// that contains the particular logic that we need (recommendationModel.es6) together 
-// with an engine (executeModel that takes that data structure and executes it.
-//
-// That obvious simplification is nice, but the conditions are still JavaScript code, 
-// which won't fit our needs for running in a non JavaScript environment. I'll need to 
-// replace the condition code with data I can interpret.
 const  executeModel = (spec, model) => {
     return model
         .filter((r) => isActive(r, spec))
