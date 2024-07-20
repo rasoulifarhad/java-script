de60b141efba37e6dc4f8472ed6b2d38acedbb35
farhad rasouli <rasouli.farhad@gmail.com>
2024-07-21 02:48:04 +0330

    updated

--- refactoring-demo/simplifying_conditional_logic/replace_command_with_function/demo2.js
+++ refactoring-demo/simplifying_conditional_logic/replace_command_with_function/demo2.js
@@ -1,15 +1,9 @@
 class ChargeCalculator {
-    constructor(customer, usage, provider) {
-    }
-
-    charge(customer, usage, provider) {
-        const baseCharge = customer.baseRate * usage;
-        return baseCharge + provider.connectionCharge;
-    }
 }
 
 function charge(customer, usage, provider) {
-    return new ChargeCalculator().charge(customer, usage, provider);
+    const baseCharge = customer.baseRate * usage;
+    return baseCharge + provider.connectionCharge;
 }
 
 
