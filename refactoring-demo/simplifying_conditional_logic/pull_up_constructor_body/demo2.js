d5a034b86688760d30fe1f92813608b90cc70f85
farhad rasouli <rasouli.farhad@gmail.com>
2024-07-23 03:28:37 +0330

    updated

--- refactoring-demo/simplifying_conditional_logic/pull_up_constructor_body/demo2.js
+++ refactoring-demo/simplifying_conditional_logic/pull_up_constructor_body/demo2.js
@@ -1,12 +1,14 @@
 class Party {
 
+    constructor(name) {
+        this._name = name;
+    }
 }
 
 class Employee extends Party {
 
     constructor(name, id, monthlyCost) {
-        super();
-        this._name = name;
+        super(name);
         this._id = id;
         this._monthlyCost = monthlyCost;
     }
