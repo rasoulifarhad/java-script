efb5d5d17919351f53a140747928228c57974cad
farhad rasouli <rasouli.farhad@gmail.com>
2024-07-23 03:35:30 +0330

    updated

--- refactoring-demo/simplifying_conditional_logic/pull_up_constructor_body/demo2.js
+++ refactoring-demo/simplifying_conditional_logic/pull_up_constructor_body/demo2.js
@@ -12,6 +12,14 @@ class Employee extends Party {
         this._id = id;
         this._monthlyCost = monthlyCost;
     }
+
+    get isPrivileged() {
+        return true;
+    }
+
+    get assignCar() {
+        return true;
+    }
 }
 
 class Department extends Party {
@@ -19,5 +27,20 @@ class Department extends Party {
     constructor(name, staff) {
         super(name);
         this._staff = staff;
+    }
+}
+
+class Manager extends Employee {
+
+    constructor(name, grade) {
+        super(name);
+        this._grade = grade;
+        if(this.isPrivileged) {
+            this.assignCar();
+        }
+    }
+
+    get isPrivileged() {
+        return this._grade > 4;
     }
 }
