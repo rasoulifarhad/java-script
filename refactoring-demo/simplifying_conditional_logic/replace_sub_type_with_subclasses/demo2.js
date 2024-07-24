23742c6ffe4bce90fe7557389483cf23260aedff
farhad rasouli <rasouli.farhad@gmail.com>
2024-07-25 00:36:52 +0330

    update

--- refactoring-demo/simplifying_conditional_logic/replace_sub_type_with_subclasses/demo2.js
+++ refactoring-demo/simplifying_conditional_logic/replace_sub_type_with_subclasses/demo2.js
@@ -17,8 +17,8 @@ class Employee {
     get type() {
         return this._type;
     }
-    
+
     toString() {
-        return `${this._name} (${this._type})`;
+        return `${this._name} (${this.type})`;
     }
 }
