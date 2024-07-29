2999e7823cb74906bdf6dcaf21835c19f2c426bf
farhad rasouli <rasouli.farhad@gmail.com>
2024-07-29 17:32:01 +0330

    updated

--- refactoring-demo/simplifying_conditional_logic/replacing_a_hierarchy/demo2.js
+++ refactoring-demo/simplifying_conditional_logic/replacing_a_hierarchy/demo2.js
@@ -53,23 +53,6 @@ class AfricanShallowDelegate {
     }
 }
 
-class NorwegianBlueParrot extends Bird {
-
-    constructor(data) {
-        super(data);
-        this._voltage = data.voltage;
-        this._isNailed = data.isNailed;
-    }
-
-    get plumage() {
-        return this._speciesDelegate.plumage;
-    }
-
-    get airSpeedVelocity() {
-        return this._speciesDelegate.airSpeedVelocity;
-    }
-}
-
 class NorwegianBlueParrotDelegate {
 
     constructor(data) {
