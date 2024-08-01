9619f256f255fd8bca125a79bdb0346e03374f82
farhad rasouli <rasouli.farhad@gmail.com>
2024-08-01 17:04:42 +0330

    updated

--- refactoring-demo/simplifying_conditional_logic/replace_superclass_with_delegate/demo2.js
+++ refactoring-demo/simplifying_conditional_logic/replace_superclass_with_delegate/demo2.js
@@ -34,6 +34,18 @@ class Scroll extends CatalogItem {
         this._lastCleaned = dateLastCleaned;
     }
 
+    get id() {
+        return this._catalogItem.id;
+    }
+
+    get title() {
+        return this._catalogItem.title;
+    }
+
+    hasTag(tag) {
+        return this._catalogItem.hasTag(tag);
+    }
+
     needsCleaning(targetDate) {
         const threshold = this.hasTag("revered") ? 700 : 1500;
         return this.daysSinceLastCleaning(targetDate) > threshold;
