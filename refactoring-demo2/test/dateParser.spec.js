09e5471043e6b790c4bc191d7e6fa91eb20d6ca3
farhad rasouli <rasouli.farhad@gmail.com>
2024-08-02 00:39:37 +0330

    updated

---
+++ refactoring-demo2/test/dateParser.spec.js
@@ -0,0 +1,26 @@
+import { expect } from "chai";
+import DateParser from "../app/date_parser/dateParser";
+
+describe("DateParser", function() {
+
+    it("parse valid date", function() {
+
+    });
+
+    it("parse valid date time", function() {
+
+    });
+
+    it("throws exception if year string can not be parsed", function() {
+
+    });
+
+    it("throws exception if year is not an integer", function() {
+
+    });
+
+    it("throws exception if year is less than 2000", function() {
+
+    });
+
+});
