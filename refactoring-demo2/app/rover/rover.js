6462b1009ce0ad6148e7f36be891966f815431f7
farhad rasouli <rasouli.farhad@gmail.com>
2024-08-02 11:13:02 +0330

    updated

--- refactoring-demo2/app/rover/rover.js
+++ refactoring-demo2/app/rover/rover.js
@@ -12,12 +12,59 @@ export default class Rover {
         for(let i = 0; i < instructions.length; i++) {
             const command = instructions.charAt(i);
             if (command === "M") {
-
+                switch (this._direction) {
+                    case "N":
+                        this._y++;
+                        break;
+                    case "S":
+                        this._y--;
+                        break;
+                    case "E":
+                        this._x++;
+                        break;
+                    case "W":
+                        this._x--;
+                        break;
+                    default:
+                        throw new Error("wrong direction");
+                }
             }
             if(command === "L") {
+                switch (this._direction) {
+                    case "N":
+                        this._direction = "W";
+                        break;
+                    case "S":
+                        this._direction = "E";
+                        break;
+                    case "E":
+                        this._direction = "N";
+                        break;
+                    case "W":
+                        this._direction = "S";
+                        break;
+                    default:
+                        throw new Error("wrong direction");
+                }
 
             }
             if(command === "R") {
+                switch (this._direction) {
+                    case "N":
+                        this._direction = "E";
+                        break;
+                    case "S":
+                        this._direction = "W";
+                        break;
+                    case "E":
+                        this._direction = "S";
+                        break;
+                    case "W":
+                        this._direction = "N";
+                        break;
+                    default:
+                        throw new Error("wrong direction");
+                }
 
             }
         }
