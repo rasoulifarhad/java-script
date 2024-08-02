a586a891f0846dabd56e94ec80c70c7cf4cbbec6
farhad rasouli <rasouli.farhad@gmail.com>
2024-08-02 11:15:42 +0330

    updated

---
+++ refactoring-demo2/test/rover.spec.js
@@ -0,0 +1,11 @@
+import { expect } from "chai";
+import Position from "../app/rover/position";
+import Rover from "../app/rover/rover";
+
+describe("Rover", function() {
+
+
+    it("", function() {
+        expect(true).to.be.false;
+    });
+});
