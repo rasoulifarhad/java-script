3e441ca74775438bb031a768a5fd4d880e9d1ae1
farhad rasouli <rasouli.farhad@gmail.com>
2024-08-02 05:33:58 +0330

    updated

--- refactoring-demo2/app/order_receipt/orderReceipt.js
+++ refactoring-demo2/app/order_receipt/orderReceipt.js
@@ -6,5 +6,43 @@ export default class OrderReceipt {
 
     printReceipt() {
         
+        let output = "";
+
+        // headers
+        output += "======Printing Orders======\n";
+
+        output += "Date - " + this._o.date;
+        output += this._o.customerName();
+        output += this._o.customerAddress();
+
+        // lineItems
+
+        let totalSalesTx = 0.0;
+        let tot = 0.0;
+
+        this._o.lineItems().forEach((lineItem) => {
+            output += lineItem.description();
+            output += '\t' ;
+            output += lineItem.price();
+            output += '\t' ;
+            output += lineItem.quantity();
+            output += '\t' ;
+            output += lineItem.totalAmount();
+            output += '\n' ;
+
+            // calculate sales tax, rate: 10%
+            let salesTax = lineItem.totalAmount() * 0.10;
+            totalSalesTx  += salesTax;
+
+            // calculate total amount of linr item =price * quantity + 10 % sales tax
+            tot += lineItem.totalAmount() + salesTax;
+        });
+        
+        // print tax
+        output += "Sales Tax" + '\t' + totalSalesTx;
+
+        // total amount
+        output += "Total Amount" + '\t' + tot;
+        return output.toString();
     }
 }
