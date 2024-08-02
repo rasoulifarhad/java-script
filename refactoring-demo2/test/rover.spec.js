74e8a859c172e55e78f32d950ac9efe749a4ca62
farhad rasouli <rasouli.farhad@gmail.com>
2024-08-02 11:20:22 +0330

    updated

--- refactoring-demo2/test/rover.spec.js
+++ refactoring-demo2/test/rover.spec.js
@@ -5,7 +5,11 @@ import Rover from "../app/rover/rover";
 describe("Rover", function() {
 
 
-    it("", function() {
-        expect(true).to.be.false;
+    it("navigate", function() {
+        const rover = new Rover(1, 1, "N");
+
+        const actualPosition = rover.navigate("RMLM");
+
+        expect(actualPosition).to.deep.equal(new Position(2, 2, "N"));
     });
 });
