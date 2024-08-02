6a56ac9977e3374af29b4552164aa793a4285458
farhad rasouli <rasouli.farhad@gmail.com>
2024-08-02 10:54:49 +0330

    updated

--- refactoring-demo2/test/orderReceipt.spec.js
+++ refactoring-demo2/test/orderReceipt.spec.js
@@ -1,4 +1,5 @@
 // import LineItem from "../app/order_receipt/lineItem";
+import LineItem from "../app/order_receipt/lineItem";
 import Order from "../app/order_receipt/order";
 import OrderReceipt from "../app/order_receipt/orderReceipt";
 import { expect } from "chai";
@@ -13,6 +14,56 @@ describe("OrderReceipt", function() {
 
         expect(output).to.include("customer #1");
         expect(output).to.include("address #1");
+    });
+
+    it("print line item", function() {
+
+        const items = [
+            new LineItem("product #1", 10.0, 2),
+            new LineItem("product #2", 5.0, 5),
+            new LineItem("product #3", 20.0, 1),
+        ];
+
+        const order = new Order("customer #2", "address #2", items);
+        const receipt = new OrderReceipt(order);
+
+        const output = receipt.printReceipt();
+
+        expect(output).to.include("product #1\t10\t2\t20\n");
+        expect(output).to.include("product #2\t5\t5\t25\n");
+        expect(output).to.include("product #3\t20\t1\t20\n");
+    });
+
+    it("print sales tax", function() {
+
+        const items = [
+            new LineItem("product #1", 10.0, 2),
+            new LineItem("product #2", 5.0, 5),
+            new LineItem("product #3", 20.0, 1),
+        ];
+
+        const order = new Order("customer #2", "address #2", items);
+        const receipt = new OrderReceipt(order);
+
+        const output = receipt.printReceipt();
+
+        expect(output).to.include("Sales Tax\t6.5");
+    });
+
+    it("print total amount", function() {
+
+        const items = [
+            new LineItem("product #1", 10.0, 2),
+            new LineItem("product #2", 5.0, 5),
+            new LineItem("product #3", 20.0, 1),
+        ];
+
+        const order = new Order("customer #2", "address #2", items);
+        const receipt = new OrderReceipt(order);
+
+        const output = receipt.printReceipt();
+
+        expect(output).to.include("Total Amount\t71.5");
     });
 
 });
